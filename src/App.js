bed86add0d8690b755d7fb9b7658e4e80d338b73
Martin Secker <martin@secker.no>
2024-10-10 15:34:04 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-// App.js
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/main';
 import Panel from './pages/panel';
 import NotFound from './pages/404';
-import Chatbot from './components/Chatbot'; // Import the Chatbot component
+
 
 function App() {
   return (
@@ -12,10 +11,11 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/panel" element={<Panel />} />
+
+
         {/* 404 route, always placed at the end */}
         <Route path="*" element={<NotFound />} />
       </Routes>
-      <Chatbot /> {/* Include the Chatbot component */}
     </BrowserRouter>
   );
 }
